4678255fe6a4080af461b7509d8cc0b9cc8c6fb5
avrahamGol <avragol@gmail.com>
2023-06-05 17:31:46 +0300

    initial data

---
+++ initialData/initialData.js
@@ -0,0 +1,57 @@
+const { users, cards } = require('./initialData.json');
+const { createCard, getAllCards } = require('../cards/models/cardAccessData');
+const { registerUser, getAllUsers } = require('../users/models/userAccessData');
+const { registerUserValidation } = require('../validation/userValidationService');
+const { createCardValidation } = require('../validation/cardsValidationService');
+const normalizeCard = require('../cards/helpers/normalizeCardService');
+const normalizeUser = require('../users/helpers/normalizeUserService');
+const hashService = require('../utils/hash/hashService');
+
+const initialData = async () => {
+    try {
+        const cardsFromDB = await getAllCards();
+        const usersFromDB = await getAllUsers();
+        if (usersFromDB.length === 0) await initUsers();
+        if (cardsFromDB.length === 0) await initCards(await getSomeId());
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const getSomeId = async () => {
+    const usersFromDB = await getAllUsers();
+    for (let i = 0; i < usersFromDB.length; i++) {
+        if (usersFromDB[i].isBiz === true) {
+            return usersFromDB[i]._id.toHexString();
+        }
+    }
+    return "6469e1f355ab41ab2413a852";
+};
+
+
+const initCards = async (userId) => {
+    cards.forEach(async card => {
+        try {
+            const normalCard = await normalizeCard(card, userId)
+            await createCardValidation(normalCard);
+            await createCard(normalCard);
+        } catch (err) {
+            console.log(err);
+        }
+    })
+};
+
+const initUsers = async () => {
+    users.forEach(async user => {
+        try {
+            const normalUser = await normalizeUser(user);
+            await registerUserValidation(normalUser);
+            normalUser.password = await hashService.generateHash(normalUser.password);
+            await registerUser(normalUser);
+        } catch (err) {
+            console.log(err);
+        }
+    })
+};
+
+module.exports = initialData;
