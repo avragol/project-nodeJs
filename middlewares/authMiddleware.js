9bec7aa182ddf904e509d9982dc87e287ec11b78
avrahamGol <avragol@gmail.com>
2023-06-20 10:46:48 +0300

    fix authmiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,10 +3,12 @@ const handleError = require('../utils/handleError');
 
 const authMiddleware = async (req, res, next) => {
     try {
-        if (!req.headers["auth-token"]) handleError(res, "please provide token", 401)
-        const userData = await verifyToken(req.headers["auth-token"]);
-        req.userData = userData;
-        next();
+        if (!req.headers["auth-token"]) { handleError(res, "please provide token", 401) }
+        else {
+            const userData = await verifyToken(req.headers["auth-token"]);
+            req.userData = userData;
+            next();
+        }
     } catch (err) {
         handleError(res, err.message, 401)
     }
--- models/mongoDB/users/usersService.js
+++ models/mongoDB/users/usersService.js
@@ -6,23 +6,23 @@ const registerUser = userData => {
 };
 
 const getUserByEmail = (email) => {
-    return User.findOne({ email }).select(["-createdAt", "-__v"]);
+    return User.findOne({ email });
 };
 
 const getAllUsers = () => {
-    return User.find().select(["-password", "-createdAt", "-__v"]);
+    return User.find().select(["-password"]);
 }
 
 const getUserById = id => {
-    return User.findById(id).select(["-password", "-createdAt", "-__v"]);
+    return User.findById(id).select(["-password"]);
 }
 
 const updateUser = (id, userToUpdate) => {
-    return User.findByIdAndUpdate(id, userToUpdate, { new: true }).select(["-password", "-createdAt", "-__v"]);
+    return User.findByIdAndUpdate(id, userToUpdate, { new: true }).select(["-password"]);
 }
 
 const updateBizUser = (id) => {
-    return User.findByIdAndUpdate(id, [{ "$set": { "isBiz": { "$not": "$isBiz" } } }], { new: true }).select(["-password", "-createdAt", "-__v"]);
+    return User.findByIdAndUpdate(id, [{ "$set": { "isBiz": { "$not": "$isBiz" } } }], { new: true }).select(["-password"]);
 };
 
 const deleteUser = (id) => {
