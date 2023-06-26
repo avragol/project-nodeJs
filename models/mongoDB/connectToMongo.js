d0572ab42fc7ebf7b0583e6ebcde58c4c2bcd2d2
Avraham Gol <121874162+avragol@users.noreply.github.com>
2023-06-26 10:23:57 +0300

    Update connectToMongo.js

--- models/mongoDB/connectToMongo.js
+++ models/mongoDB/connectToMongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const cofig = require("config");
 
 const connectToMongo = () => {
-    return mongoose.connect(config.get("dbConfig.url");
+    return mongoose.connect(config.get("dbConfig.url"));
 };
 
 module.exports = connectToMongo;
